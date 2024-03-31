bbd9ad0f3bfcc35e98ae43eb66d826d9cd16d47b
Joey Schutz <schutzjoey@gmail.com>
2024-03-31 01:01:44 -0400

    can submit a racer

--- boshi-olympics/racer-registration.js
+++ boshi-olympics/racer-registration.js
@@ -1,28 +1,37 @@
-function submitRacer() {
+const nameEntryId = 'entry.674049401';
+const speedEntryId = 'entry.1074850540';
+
+
+async function submitRacer() {
     let name = document.getElementById('name').value;
     let speed = document.getElementById('speed').value;
 
-    var details = {
-        'name': name,
-        'speed': speed
-    };
+    // var details = {
+    //     'name': name,
+    //     'speed': speed
+    // };
     
-    var formBody = [];
-    for (var property in details) {
-      var encodedKey = encodeURIComponent(property);
-      var encodedValue = encodeURIComponent(details[property]);
-      formBody.push(encodedKey + "=" + encodedValue);
-    }
-    formBody = formBody.join("&");
+    // var formBody = [];
+    // for (var property in details) {
+    //   var encodedKey = encodeURIComponent(property);
+    //   var encodedValue = encodeURIComponent(details[property]);
+    //   formBody.push(encodedKey + "=" + encodedValue);
+    // }
+    // formBody = formBody.join("&");
     
-    fetch('https://docs.google.com/forms/u/2/d/e/1FAIpQLSenDKu3U4DCLzprMmBJFPXinZLHQBXefLr1_2kZE5xpQ91tMQ/formResponse', {
+    await fetch('https://docs.google.com/forms/u/2/d/e/1FAIpQLSenDKu3U4DCLzprMmBJFPXinZLHQBXefLr1_2kZE5xpQ91tMQ/formResponse', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: formBody
+      body: new URLSearchParams({
+        'entry.674049401': name,
+        'entry.1074850540': speed
+      }),
+      mode: 'no-cors'
     })
-
+    
+    window.location.href ="/boshi-olympics/racer-submitted.html";
     console.log(`submitting racer with name ${name} and speed ${speed}`);
 }
 
