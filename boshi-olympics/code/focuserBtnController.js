5f6a8c8601215ef263daf2bbb18e282b003043d5
Joey Schutz <schutzjoey@gmail.com>
2024-04-03 21:55:52 -0400

    focuser cheer button position is random

--- boshi-olympics/code/focuserBtnController.js
+++ boshi-olympics/code/focuserBtnController.js
@@ -2,6 +2,10 @@ var cheerButton;
 var hideButtonIntervalMilliseconds;
 var lastButtonShowTime;
 
+function randomRange(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
 function hideCheerButton() {
     console.log('hiding cheer btn');
     cheerButton.style.display = 'none';
@@ -17,12 +21,15 @@ function hideCheerButton() {
 function showCheerButton() {
     console.log('showing cheer btn');
     lastButtonShowTime = Date.now();
+
+    cheerButton.style.marginLeft = `${randomRange(0, 60)}%`;
+    cheerButton.style.marginTop  = `${randomRange(0, 200)}px`;
     cheerButton.style.display = 'block';
 }
 
 
 function initFocuserBtnController() {
     cheerButton = document.getElementById('cheer-btn');
-    hideButtonIntervalMilliseconds = 100;
+    hideButtonIntervalMilliseconds = 10;
     lastButtonShowTime = undefined;
 }
