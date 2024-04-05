ff20b3ef7c4b015d39f163da68f55ee438ed8060
Joey Schutz <schutzjoey@gmail.com>
2024-04-05 16:27:20 -0400

    updated religion description

--- boshi-olympics/code/racerTypeSelections.js
+++ boshi-olympics/code/racerTypeSelections.js
@@ -1,5 +1,5 @@
 var racerTypeDescriptions = {
-    'religious': 'at the beginning of the race, before you start to cheer you can pray to the boshi gods for support. for each prayer, increase your speed by +2',
+    'religious': 'at the beginning of the race, before you start to cheer you can pray to the boshi gods for support. for each prayer, increase your speed by +3',
     'crowdPleaser': 'for each cheer, move speed * X/2, where X is the number of different people who have cheered for you this race',
     'loner': 'for each cheer, move speed * (X - Y), where X can be upgraded, and Y is the number of different people who have cheered for you this race',
     'speedster': 'for each cheer, move speed * 2. additionally, you can upgrade your speed +2 for 1 attribute point. however, every successive cheer has to be done twice as fast as the previous one (e.g. if you wait 1 second between the first two cheers, you must then cheer a third time within 0.5 seconds, a fourth time within 0.25 seconds, and so on. once you fail, a determination roll will be made. if successful, your cheer timer will be reset and you may continue as you were. if unsuccessful, your speed will be set to 1.',
