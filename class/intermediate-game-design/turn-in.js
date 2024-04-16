b46c805114d0dd4bf897d73094f3d7fffe247962
Joey Schutz <schutzjoey@gmail.com>
2024-04-16 16:44:26 -0400

    warnings

--- class/intermediate-game-design/turn-in.js
+++ class/intermediate-game-design/turn-in.js
@@ -6,10 +6,31 @@ function submit() {
     let teamwork = document.getElementById('teamwork').value;
     let help = document.getElementById('help').value;
 
-    console.log('name: ' + name);
-    console.log('reflection: ' + reflection);
-    console.log('teamwork: ' + teamwork);
-    console.log('help: ' + help);
+    let showedWarning = false;
+    if(name == undefined || name == null || name == '') {
+        let warning = document.getElementById('nameWarning');
+        warning.style.display = 'block';
+        showedWarning = true;
+    }
+    if(reflection == undefined || reflection == null || reflection == '') {
+        let warning = document.getElementById('reflectionWarning');
+        warning.style.display = 'block';
+        showedWarning = true;
+    }
+    if(teamwork == undefined || teamwork == null || teamwork == '') {
+        let warning = document.getElementById('teamworkWarning');
+        warning.style.display = 'block';
+        showedWarning = true;
+    }
+    if(help == undefined || help == null || help == '') {
+        let warning = document.getElementById('helpWarning');
+        warning.style.display = 'block';
+        showedWarning = true;
+    }
+
+    
+    let warning = document.getElementById('submitWarning');
+    warning.style.display = showedWarning? 'block': 'none';
 }
 
 
