4413412e593a083b15d223256d8e703db9fbb584
Joey Schutz <schutzjoey@gmail.com>
2024-04-25 11:21:16 -0400

    better closing thoughts

--- class/intermediate-game-design/showAssignmentBrief.js
+++ class/intermediate-game-design/showAssignmentBrief.js
@@ -6,6 +6,25 @@ function sayWelcome() {
     assignmentBrief.style.display = 'block';
 }
 
+
+function sayImReady() {
+    let imReadyBtn = document.getElementById('imReadyBtn');
+    let assignmentBrief = document.getElementById('fullAssignmentBrief');
+
+    imReadyBtn.style.display = 'none';
+    assignmentBrief.style.display = 'block';
+}
+
+
+function makeAnything() {
+    let makeAnythingBtn = document.getElementById('makeAnythingBtn');
+    let noteFromJoey = document.getElementById('noteFromJoey');
+
+    makeAnythingBtn.style.display = 'none';
+    noteFromJoey.style.display = 'block';
+}
+
+
 function promiseFun() {
     let promiseBtn = document.getElementById('promiseBtn');
     let part2 = document.getElementById('phase2');
@@ -26,5 +45,11 @@ function promiseFun() {
 
         let promiseBtn = document.getElementById('promiseBtn');
         promiseBtn.addEventListener('click', promiseFun);
+
+        let imReadyBtn = document.getElementById('imReadyBtn');
+        imReadyBtn.addEventListener('click', sayImReady);
+
+        let makeAnythingBtn = document.getElementById('makeAnythingBtn');
+        makeAnythingBtn.addEventListener('click', makeAnything);
     }
 })(window, document, undefined);
